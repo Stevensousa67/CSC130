afbe0298ff2b944d9bd8881622a06848fd31ae7f
Steven Sousa <silver.steven67@gmail.com>
2021-02-13 22:06:25 -0500

    exercise 3 and 4 done

--- src/Chapter2_Exercise3.java
+++ src/Chapter2_Exercise3.java
@@ -8,12 +8,41 @@ public class Chapter2_Exercise3 {
 		
 		System.out.println("Exercise 2.3");
 		
-		/** 2) Create variables named hour, minute, and second. Assign values that are roughly
+		/* 2) Create variables named hour, minute, and second. Assign values that are roughly
 		 * the current time. Use a 24-hour clock so that at 2:00PM the value of hour is 14. 
 		 * 
 		 */
 		
+		int hour = 21;
+		int minute = 10;
+		int second = 23;
 		
+		/* 3) Make the program calculate and display the number of seconds since midnight
+		 * 
+		 */
 
+		int hour_to_seconds = hour*3600;
+		int minute_to_seconds = minute*60;
+		int total_seconds_since_midnight = hour_to_seconds + minute_to_seconds + second;
+		
+		System.out.println("It has passed " + total_seconds_since_midnight + " seconds since midnight");
+		
+		/* 4) Calculate and display the number of seconds remaining in the day
+		 * 
+		 */
+		
+		int total_seconds_day = 86399;
+		int seconds_left_day = total_seconds_day - total_seconds_since_midnight;
+		
+		System.out.println("There are " + seconds_left_day + " seconds left in the day.");
+		
+		/* 5) Calculate and display the percentage of the day that has passed. You might run into
+		 * problems when computing percentages with integers, so consider using floating-points
+		 * 
+		 */
+		
+		
+		
+		
 	}// end main
 }// end class
