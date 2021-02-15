6da2bb6fddef1fcb5b83a34192ec52fe044b20f9
Steven Sousa <silver.steven67@gmail.com>
2021-02-15 14:08:50 -0500

    Exercise 4 done

--- src/Chapter3_Exercise4.java
+++ src/Chapter3_Exercise4.java
@@ -37,20 +37,31 @@ public class Chapter3_Exercise4 {
 		Scanner input = new Scanner(System.in);
         int number = random.nextInt(100) + 1;
         
+        /* 4) Read the user input as an integer and display the result. Again, compile and test.
+         * 
+         */
+        
         System.out.println("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?");
         System.out.println("Type a number: ");
         int user_number = input.nextInt();
+        System.out.println("Your guess is: " + user_number);
+        
+       
+        /* 5) Compute and display the difference between the user's guess and the number that was generated.
+         * 
+         */
         
         int difference = number - user_number;
         
         // in case random number is smaller than user number, the difference won't be negative.
         if (difference < 0) {
         	difference = difference * -1;
-        }
-        
-        System.out.println("Your guess is: " + user_number);
+        }// end if
+
         System.out.println("The number I was thinking of is: " + number);
         System.out.println("You were off by: " + difference);
+        
+        
 		
 	}// end class
 }// end class
